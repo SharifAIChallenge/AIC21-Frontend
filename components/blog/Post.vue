ace4cb10dc9d0a9e0704e828d003d4551a10e2a6
saman-re <68374661+saman-re@users.noreply.github.com>
2021-01-07 22:44:48 +0330

    Post component added

---
+++ components/blog/Post.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+    <h2>{{post.title}}</h2>
+    <h4><v-avatar color="primary" size="40"></v-avatar>{{post.author}}</h4>
+    <p>{{post.description.slice(0,20)}}...</p>
+    <p><i>{{post.date}}</i></p>
+    <button>save!</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['post'],
+}
+</script>
