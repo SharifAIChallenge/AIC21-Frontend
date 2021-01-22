445a1b29fba021233e1eacdf922443aecb0661e3
jamshidi.m799 <jamshidi.m799@gmail.com>
2021-01-22 22:48:43 +0330

    refactor: NOTIFICATION

--- store/notification.js
+++ store/notification.js
@@ -1,11 +1,21 @@
+import { getNotifications } from '~/api';
+
 export const state = () => ({
   notifications: [],
-})
+});
+
+export const actions = {
+  async get({ commit }) {
+    this.$axios.$get('/subscribe/public-notifications').then(res => {
+      commit('set', res);
+    });
+  },
+};
 
 export const mutations = {
   set(state, { notifications, status_code }) {
     if (status_code === 200) {
-      state.notifications = notifications.reverse()
+      state.notifications = notifications.reverse();
     }
   },
-}
+};
