f1ae44b686c0dd70d0d405f4c83831ee03ee9e3f
saman-re <saman.rezapoor1379@yahoo.com>
2021-02-16 16:57:10 +0330

    some change in footer

--- components/MyFooter.vue
+++ components/MyFooter.vue
@@ -1,15 +1,7 @@
 <template>
   <v-footer dark padless>
     <v-card-text class="justify-center d-flex">
-      <v-btn
-        v-for="social in socials"
-        :key="social.icon"
-        :href="social.url"
-        class="mx-4 white--text"
-        target="_blank"
-        icon
-        large
-      >
+      <v-btn v-for="social in socials" :key="social.icon" :href="social.url" class="mx-4 transparent primary--text" target="_blank" icon large>
         <v-icon size="24px">
           {{ social.icon }}
         </v-icon>
@@ -17,17 +9,19 @@
     </v-card-text>
 
     <v-card-text class="white--text align-center justify-space-between d-flex mx-sm-3">
-      <logo-mark />
-      <div>
-        {{ new Date().getFullYear() }} —
-        <strong>AI Challenge @ SSC</strong>
+      <div dir="ltr" class="primary--text">
+        <strong>
+          <v-icon right small class="primary--text">mdi-copyright</v-icon>
+          2021 Sharif AI Challenge
+        </strong>
       </div>
+      <logo-mark />
     </v-card-text>
   </v-footer>
 </template>
 
 <script>
-import LogoMark from '../components/LogoMark'
+import LogoMark from '../components/LogoMark';
 
 export default {
   components: { LogoMark },
@@ -35,21 +29,33 @@ export default {
     return {
       socials: [
         {
-          icon: 'mdi-telegram',
-          url: 'https://t.me/aichallenge',
+          icon: 'mdi-twitch',
+          url: '/',
+        },
+        {
+          icon: 'mdi-linkedin',
+          url: '/',
+        },
+        {
+          icon: 'mdi-instagram',
+          url: 'http://www.instagram.com/AiChallenge',
+        },
+        {
+          icon: 'mdi-github',
+          url: '/',
         },
         {
           icon: 'mdi-twitter',
           url: 'http://www.twitter.com/AiChallenge',
         },
         {
-          icon: 'mdi-instagram',
-          url: 'http://www.instagram.com/AiChallenge',
+          icon: 'mdi-telegram',
+          url: 'https://t.me/aichallenge',
         },
       ],
-    }
+    };
   },
-}
+};
 </script>
 
 <style scoped></style>
