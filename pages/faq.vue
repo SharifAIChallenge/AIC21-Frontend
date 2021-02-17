5ddc55fe62ca8ca99a259b177f9150cf5e111ac9
hessamasna <hessamasna@yahoo.com>
2021-02-17 16:51:47 +0330

    should check

--- pages/faq.vue
+++ pages/faq.vue
@@ -2,7 +2,7 @@
   <v-app>
     <div class="first-slider" style="position: relative;">
       <div class="slider-text">
-        <div style="margin-top: 150px; font-size: 6rem;">
+        <div style="margin-top: 20vh; font-size: 6rem;">
           سوالات متداول
         </div>
         <div style="font-size: 50px;">
@@ -15,22 +15,43 @@
           و قوانین آن طراحی شده است
         </div>
       </div>
-    <div class="vl"></div>
+      <div class="vl"></div>
     </div>
     <div class="titles">
-
-    <div>
-      sghllsdcs
+      <div class="title">
+        عمومی
+      </div>
+      <div class="title">
+        زمان بندی
+      </div>
+      <div class="title">
+        مسابقه
+      </div>
     </div>
-        <div>
-      sghllsdcs
+    <div class="faq-container">
+      <div class="faq-card">
+        <div class="faq-card-title">
+          <v-icon left color="primary" size="55">
+            mdi-calendar
+          </v-icon>
+          نبرد هوش مصنوعی چگونه کار می کند
+        </div>
+        <span class="faq-card-text">
+          cwewedw ed eeeeeeeeeeeeeeeییییeeeeeeeee web kitd wedw edw ed wedd
+        </span>
+      </div>
+      <div class="faq-card">
+        <div class="faq-card-title">
+          <v-icon left color="primary" size="55">
+            mdi-calendar
+          </v-icon>
+          نبرد هوش مصنوعی چگونه کار می کند
+        </div>
+        <span class="faq-card-text">
+          cwewedw ed eeeeeeeeeeeeeeeییییeeeeeeeee web kitd wedw edw ed wedd
+        </span>
+      </div>
     </div>
-        <div>
-      sghllsdcs
-    </div>
-    </div>
-
-
   </v-app>
 </template>
 
@@ -38,7 +59,6 @@
 export default {
   layout: 'landing',
   auth: false,
-
 };
 </script>
 
@@ -57,11 +77,40 @@ export default {
   position: absolute;
   left: 49%;
   bottom: 0px;
-
 }
-.titles{
-    display: flex;
-    justify-content:space-evenly;
-
+.titles {
+  position: sticky;
+  display: flex;
+  justify-content: center;
+  height: 100px;
+  margin-top: auto;
+}
+.title {
+  position: sticky;
+  margin: auto 50px;
+}
+.faq-container {
+  display: flex;
+  justify-content: space-evenly;
+}
+.faq-card {
+  position: relative;
+  max-width: 450px !important;
+  height: 500px;
+  border: 3px solid red;
+  padding: 20px;
+  /* margin: 20px; */
+}
+.faq-card-title {
+  position: absolute;
+  display: inline-block;
+  margin-top: 50px;
+  font-size: 2rem;
+}
+.faq-card-text {
+  position: absolute;
+  margin-top: 200px;
+  margin-left: 30px;
+  margin-right: 20px;
 }
 </style>
