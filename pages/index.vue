ae5bd529fb5c082fffad0d23417118502b0aef6c
hessamasna <hessamasna@yahoo.com>
2021-02-19 21:32:10 +0330

    idk

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,16 @@
 <template>
   <div id="main">
     <Header color="transparent" />
-<<<<<<< HEAD
-    <Video color="red" />
-=======
+    <Video />
     <Intersection />
->>>>>>> 88b1cdebc202bf76f5361e24f7030bfe13624f7c
   </div>
 </template>
 
 <script>
 import Header from '~/components/landing/Header.vue';
-<<<<<<< HEAD
 import Video from '~/components/landing/video.vue'
 export default {
   components: { Header , Video},
-=======
-import Intersection from '~/components/landing/Intersection.vue'
-export default {
-  components: { Header ,Intersection },
->>>>>>> 88b1cdebc202bf76f5361e24f7030bfe13624f7c
   layout: 'landing',
   auth: false,
 };
