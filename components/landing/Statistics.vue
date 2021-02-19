1cbdaebb4e51715f2a9c1998a89bc6f1bde835e3
saman-re <saman.rezapoor1379@yahoo.com>
2021-02-19 16:59:53 +0330

    some change in statistics mobile style

--- components/landing/Statistics.vue
+++ components/landing/Statistics.vue
@@ -1,12 +1,11 @@
 <template>
   <v-card tile flat class="secondary my-5">
-    <v-row class="px-10">
+    <v-row class="px-10 py-3">
       <v-col lg="3" md="3" sm="6" v-for="(stat,i) in stats" :key="i">
-        <v-card-text class="d-flex flex-column px-10 py-0 text-right" style="max-width:150px">
-          <v-icon x-large right class="pb-2 black--text justify-start">{{stat.icon}}</v-icon>
-          <span class="text-h5">{{stat.part}}</span>
-          <v-divider/>
-          <span class="pt-1">{{stat.info}}</span>
+        <v-card-text class="d-flex flex-column px-10 py-0 text-right mx-auto" style="max-width:150px">
+          <v-icon x-large right class="pb-2 black--text">{{stat.icon}}</v-icon>
+          <span class="text-h5 pb-1">{{stat.part}}</span>
+          <span class="pt-1" style="border-top:1px solid white">{{stat.info}}</span>
         </v-card-text>
       </v-col>
     </v-row>
@@ -18,22 +17,22 @@ export default {
         return{
             stats:[
                 {
-                    icon:'mdi-account-box',
+                    icon:'mdi-clipboard-account',
                     part:NaN,
                     info:'شرکت کننده'
                 },
                 {
-                    icon:'mdi-account-box',
+                    icon:'mdi-account-group',
                     part:NaN,
                     info:'تیم'
                 },
                 {
-                    icon:'mdi-account-box',
+                    icon:'mdi-code-braces-box',
                     part:NaN,
                     info:'ارسال کد'
                 },
                 {
-                    icon:'mdi-account-box',
+                    icon:'mdi-sword-cross',
                     part:NaN,
                     info:'نبرد'
                 },
