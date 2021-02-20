c9121a2964c56181cbf74834526282e5e245af37
hessamasna <hessamasna@yahoo.com>
2021-02-20 17:00:05 +0330

    fix bugs

--- components/landing/video.vue
+++ components/landing/video.vue
@@ -3,27 +3,27 @@
     <img src="logo__white.svg" class="background-logo" />
 
     <v-row>
-      <v-col cols="12" sm="12" md="6" lg="6" xl="6">
+      <v-col cols="12" sm="12" md="6">
         <div class="title">
           <div>
             <span style="font-size: 1.8rem;">ثبت نام کنید، تیم تشکیل دهید</span>
-            <span style="color: transparent; border: white; -webkit-text-stroke: 2px white; font-size: 4rem;">و</span>
+            <span class="border-text-white">و</span>
           </div>
-          <div style="margin-top: 30px;  margin-bottom: 10px;">
-            <span style="color:var(--v-primary-base); font-size: 3rem; margin-bottom: 10px; font-weight: bold;">بگذارید</span>
-            <span style="color: transparent; border: white; -webkit-text-stroke: 2px var(--v-primary-base); font-size: 4rem;">
+          <div style="margin: 30px 0px 10px;">
+            <span class="text-primary-color">بگذارید</span>
+            <span class="border-text-primary">
               کدها
             </span>
-            <span style="color:var(--v-primary-base); font-size: 3rem; font-weight: bold;">بجنگند</span>
+            <span class="text-primary-color">بجنگند</span>
           </div>
           <v-row>
             <v-col>
-              <v-btn block style="border-radius: 0; font-weight: normal; margin: 15px;">
+              <v-btn block class="btn" @click="toggleShow()">
                 {{ $t('home.signUpNow') }}
               </v-btn>
             </v-col>
             <v-col>
-              <v-btn block color="black" style="border-radius: 0; font-weight: normal; margin: 15px;">
+              <v-btn block color="black" class="btn" to="/faq">
                 <v-icon style="margin: 10px;">mdi-frequently-asked-questions</v-icon>
                 {{ $t('home.faq') }}
               </v-btn>
@@ -31,7 +31,7 @@
           </v-row>
         </div>
       </v-col>
-      <v-col cols="12" sm="12" md="6" lg="6" xl="6">
+      <v-col cols="12" sm="12" md="6">
         <div class="video">
           <video width="80% " controls autoplay>
             <source src="landing.mp4" type="video/mp4" />
@@ -67,5 +67,29 @@ export default {};
   opacity: 0.5;
   transform: translateX(50%);
   z-index: 0;
+}
+.btn {
+  border-radius: 0;
+  font-weight: normal;
+  margin: 15px;
+}
+.border-text-white {
+  color: transparent;
+  border: white;
+  -webkit-text-stroke: 2px white;
+  font-size: 4rem;
+}
+.border-text-primary {
+  color: transparent;
+  border: white;
+  -webkit-text-stroke: 2px var(--v-primary-base);
+  font-size: 4rem;
+  margin-bottom: 10px;
+  margin-top: 10px;
+}
+.text-primary-color{
+  color:var(--v-primary-base);
+   font-size: 3rem;
+    font-weight: bold;
 }
 </style>
