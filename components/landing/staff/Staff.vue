a03e47476b439f4c44b0c2988049a3f301641a5f
saman-re <saman.rezapoor1379@yahoo.com>
2021-02-21 21:34:35 +0330

    staff section done

---
+++ components/landing/staff/Staff.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-container class="my-15">
+    <div class="mb-12 text-center white--text text-h3 font-weight-bold" width="100%">تیم ما</div>
+    <v-row>
+        <v-col cols="12" lg="3" md="4" sm="6" v-for="n in 4" class="d-flex justify-center px-2" :key="n">
+        <StaffCard/>
+        </v-col>
+    </v-row>
+    </v-container>
+</template>
+
+<script>
+import StaffCard from './StaffCard'
+export default {
+    components:{
+        StaffCard
+    }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@
     <Intersection />
     <TrophyDesktop class="hidden-sm-and-down"/>
     <TrophyMobile class="hidden-md-and-up"/>
+    <Staff/>
   </div>
 </template>
 
@@ -14,8 +15,9 @@ import Statistics from '~/components/landing/Statistics';
 import Intersection from '~/components/landing/Intersection.vue'
 import TrophyDesktop from '~/components/landing/TrophyDesktop';
 import TrophyMobile from '~/components/landing/TrophyMobile';
+import Staff from '~/components/landing/staff/Staff';
 export default {
-  components: { Header ,Statistics,Intersection,TrophyDesktop,TrophyMobile},
+  components: { Header ,Statistics,Intersection,TrophyDesktop,TrophyMobile,Staff},
   layout: 'landing',
   auth: false,
 };
