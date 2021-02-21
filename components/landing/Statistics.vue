2e3b1f6fa1d774688899c0a5a2218cf97a701a3a
saman-re <saman.rezapoor1379@yahoo.com>
2021-02-21 19:45:51 +0330

    change the border style of statistics info

--- components/landing/Statistics.vue
+++ components/landing/Statistics.vue
@@ -2,10 +2,10 @@
   <v-card tile flat class="secondary my-5">
     <v-row class="px-10 py-3">
       <v-col lg="3" md="3" sm="6" v-for="(stat, i) in stats" :key="i">
-        <v-card-text class="d-flex flex-column px-10 py-0 text-right mx-auto" style="max-width:150px">
+        <v-card-text class="px-10 py-0 text-right mx-auto" style="max-width:150px">
           <v-icon x-large right class="pb-2 black--text">{{ stat.icon }}</v-icon>
-          <span class="text-h5 pb-1">{{ stat.part }}</span>
-          <span class="pt-1" style="border-top:1px solid white">{{ stat.info }}</span>
+          <div><span class="text-h5 pb-1">{{ stat.part }}</span></div>
+          <div class="mt-3"><span class="pt-1" style="border-top:1px solid white">{{ stat.info }}</span></div>
         </v-card-text>
       </v-col>
     </v-row>
