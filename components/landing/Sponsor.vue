ddc767699699a049cb60d7bd4ed025e2f5bedbac
Amirzgh1 <Amirgh5665@gmail.com>
2021-02-21 22:22:43 +0330

    fixing problems sponsor

--- components/landing/Sponsor.vue
+++ components/landing/Sponsor.vue
@@ -1,77 +1,81 @@
 <template>
-  <v-app>
-      <v-row>
-          <v-col cols="6"  class="white ssc">
-              <div class="white">
-                  <img src="~assets/images\logo/ssc-logo.svg" class="sscLogo" >
-              </div>
-              
-          </v-col>
-          <v-col cols="6" align-self="center">
-            <div class="sscIntro">
-                <h4 class="">انجمن علمی دانشکده مهندسی کامپیوتر</h4>
-                <br>
-                <h3>دانشگاه صنعتی شریف</h3>
-                <br>
-                <h2>برگزار میکند</h2>
-                </div>
-          </v-col>
-      </v-row>
-      <v-row>
-          <v-col cols="6" class="digikala" align-self="center">
-              <h1>
-                  با حمایت
-              </h1>
-              <br>
-              <h2>
-                  دیجیکالا
-              </h2>
-          </v-col>
-          <v-col cols="6" class="digikalaLogoCol">
-              <div>
-                  <img src="~assets/images\logo/digikala-logo.svg" class="digikalaLogo">
-              </div>
-          </v-col>
-      </v-row>
-  </v-app>
+  <div class="mt-2">
+    <v-row>
+      <v-col md="6" class="white ssc">
+        <div class="white sscLogo">
+          <img src="~assets/images/logo/ssc-logo.svg" />
+        </div>
+      </v-col>
+      <v-col md="6" align-self="center">
+        <div class="sscIntro">
+          <h4>انجمن علمی دانشکده مهندسی کامپیوتر</h4>
+          <h3>دانشگاه صنعتی شریف</h3>
+          <h2>برگزار میکند</h2>
+        </div>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="6" class="digikala" align-self="center">
+        <h1>
+          با حمایت
+        </h1>
+        <h2>
+          دیجیکالا
+        </h2>
+      </v-col>
+      <v-col cols="6" class="digikalaLogoCol">
+        <div>
+          <img src="~assets/images/logo/digikala-logo.svg" class="digikalaLogo" />
+        </div>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-.sscIntro{
-    text-align: center;
+<style lang="scss" scoped>
+.digikalaLogoCol {
+  background-color: var(--v-primary-base);
+  div {
+    margin: auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    img {
+      width: 20vw;
+    }
+  }
 }
-.sscIntro h4{
+.digikala {
+  text-align: center;
+  h2 {
+    padding: 5px;
     color: var(--v-primary-base);
+  }
 }
-.sscIntro h3{
-    opacity: 0.9;
+.sscIntro {
+  text-align: center;
+  h4 {
+    color: var(--v-primary-base);
+  }
+  h3 {
+    opacity: 0.8;
+  }
 }
-.sscLogo{
-   width: 250px;
-   margin-top: 150px;
-   filter: invert(0%) sepia(0%) saturate(7500%) hue-rotate(327deg) brightness(0%) contrast(104%);
-}
-.digikala{
-    text-align: center;
-}
-.digikala h2{
-    color: var(--v-primary-base); 
-}
-.digikalaLogoCol{
-    background-color: var(--v-primary-base);
-    text-align: center;
-}
-.digikalaLogo{
-    margin-top: 150px;
-    width: 200px;
-}
-.ssc{
-    text-align: center;
+.ssc {
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    img {
+      width: 20vw;
+      filter: invert(0%) sepia(0%) saturate(7500%) hue-rotate(327deg) brightness(0%) contrast(104%);
+    }
+  }
 }
 </style>
