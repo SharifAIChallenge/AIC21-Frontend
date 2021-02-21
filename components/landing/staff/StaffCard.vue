ec54147c57baf83fac245cb2c1a4a68b9fc6afda
saman-re <saman.rezapoor1379@yahoo.com>
2021-02-21 21:31:53 +0330

    improvement in Staff Card component style

--- components/landing/staff/StaffCard.vue
+++ components/landing/staff/StaffCard.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="staff_card">
-      <div class="staff_img grey darken-3" style="margin-top:20px;height:185px">
+  <div class="staff_card" style="max-width:350px">
+      <div class="staff_img grey darken-3" style="margin-top:24px;height:185px">
           <img src="" alt="staff image" width="100%" height="100%">
       </div>
     <div class="staff_intro">
       <div class="font-weight-bold white--text">
         پسر خوب
       </div>
-      <div class="py-1 text-caption text-start">
+      <div class="text-caption text-start" style="margin-top:12px">
         برنامه نویس فرانت اند
       </div>
     </div>
-    <div class="primary py-1 text-center white--text font-weight-bold">
+    <div class="primary py-3 text-center white--text font-weight-black">
       فنی
     </div>
   </div>
@@ -25,6 +25,6 @@
 .staff_intro,
 .staff_img{
     width: calc(100% - 40px);
-    margin: 10px 20px;
+    margin: 24px 20px;
 }
 </style>
