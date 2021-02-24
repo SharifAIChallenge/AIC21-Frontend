da75ade27f2e5e0e53460b06b71d80f4ed411669
mansoureh <mansoureh.1998@gmail.com>
2021-02-24 18:05:07 +0330

    edit footer

--- components/MyFooter.vue
+++ components/MyFooter.vue
@@ -1,28 +1,32 @@
 <template>
   <v-footer dark padless class="footer">
-    <v-card-text class="align-center justify-space-between d-flex mx-sm-3 flex-wrap">
-      <div dir="ltr" class="primary--text">
-        <strong>
-          <v-icon right small class="primary--text">mdi-copyright</v-icon>
-          2021 Sharif AI Challenge
-        </strong>
-      </div>
-      <div class="hidden-md-and-down social_box mx-auto mb-3 justify-center">
-        <v-btn
-          v-for="social in socials"
-          :key="social.icon"
-          :href="social.url"
-          class="mx-4 transparent primary--text mx-sm-2"
-          target="_blank"
-          icon
-          large
-        >
-          <v-icon size="24px">
-            {{ social.icon }}
-          </v-icon>
-        </v-btn>
-      </div>
-      <img src="../assets/images/logo/logo__primary.svg" class="logo" />
+    <v-card-text class="align-center justify-space-between d-flex mx-md-3 flex-wrap">
+      <v-row>
+        <v-col cols="12" md="2" dir="ltr" class="primary--text d-flex align-center justify-center justify-md-start">
+          <strong>
+            <v-icon right small class="primary--text">mdi-copyright</v-icon>
+            2021 Sharif AI Challenge
+          </strong>
+        </v-col>
+        <v-col cols="12" md="8" class="hidden-md-and-down social_box mx-auto mb-3 d-flex justify-center align-center">
+          <v-btn
+            v-for="social in socials"
+            :key="social.icon"
+            :href="social.url"
+            class="mx-4 transparent primary--text mx-sm-4"
+            target="_blank"
+            icon
+            large
+          >
+            <v-icon size="30px">
+              {{ social.icon }}
+            </v-icon>
+          </v-btn>
+        </v-col>
+        <v-col cols="12" md="2" dir="ltr" class="d-flex justify-center justify-md-start">
+          <img src="../assets/images/logo/logo__primary.svg" class="logo" />
+        </v-col>
+      </v-row>
     </v-card-text>
   </v-footer>
 </template>
