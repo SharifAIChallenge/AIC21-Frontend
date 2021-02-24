1688956dd39a929bb09f43c8b3bbf305268c4239
Amirzgh1 <Amirgh5665@gmail.com>
2021-02-24 19:02:27 +0330

    working on staff

--- components/landing/staff/StaffCard.vue
+++ components/landing/staff/StaffCard.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="staff_card d-inline-block" style="max-width:350px;min-width:290px">
+    <v-container>
       <div class="staff_img grey darken-3" style="margin-top:24px;height:185px">
-          <img src="" alt="staff image" width="100%" height="100%">
+        <img src="" alt="staff image" width="100%" height="100%" />
       </div>
-    <div class="staff_intro">
-      <div class="font-weight-bold white--text">
-        پسر خوب
+      <div class="staff_intro">
+        <div class="font-weight-bold white--text">
+          پسر خوب
+        </div>
+        <div class="text-caption text-start" style="margin-top:12px">
+          برنامه نویس فرانت اند
+        </div>
       </div>
-      <div class="text-caption text-start" style="margin-top:12px">
-        برنامه نویس فرانت اند
+      <div class="primary py-1 text-center white--text font-weight-black">
+        فنی
       </div>
-    </div>
-    <div class="primary py-1 text-center white--text font-weight-black">
-      فنی
-    </div>
+    </v-container>
   </div>
 </template>
 
@@ -23,8 +25,8 @@
   border: 3px solid var(--v-primary-base);
 }
 .staff_intro,
-.staff_img{
-    width: calc(100% - 40px);
-    margin: 24px 20px;
+.staff_img {
+  width: calc(100% - 40px);
+  margin: 24px 20px;
 }
 </style>
---
+++ components/staffs/StaffIntro.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div class="techDiv">
+      <h1 class="text-right">فنی</h1>
+      <div class="text-left">
+        <span class="px-5">گرافیک</span>
+        <span class="px-5">سایت</span>
+        <span class="px-5">زیرساخت</span>
+        <span class="px-5">سرور و کلاینت</span>
+      </div>
+    </div>
+    <!-- <TechTeam /> -->
+    <div class="marketing mt-15">
+      <h1 class="text-right">برندینگ</h1>
+      <div class="text-left">
+        <span class="px-5">محتوا</span>
+        <span class="px-5">مدیا</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import TechTeam from './TechTeam';
+export default {
+  props: ['staffs'],
+  data() {
+    return {
+      techStaff: [],
+    };
+  },
+  components: {
+    TechTeam,
+  },
+  methods: {},
+  mounted() {
+    for (var prop in this.staffs) {
+      this.techStaff.push(this.staffs.tech)
+      console.log(this.techStaff)
+    }
+  },
+};
+</script>
+<style lang="scss" scoped>
+.techDiv,
+.marketing {
+  height: 10vw;
+  display: flex;
+  align-items: center;
+  background-color: var(--v-primary-base);
+  h1 {
+    justify-content: flex-start;
+    text-align: right;
+    margin: auto;
+    font-size: 3rem;
+  }
+  div {
+    text-align: left;
+    justify-content: left;
+    margin: auto;
+  }
+}
+</style>
