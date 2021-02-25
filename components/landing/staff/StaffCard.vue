b898962b497eccf55062a4df0e8d35f98e606dd0
saman-re <saman.rezapoor1379@yahoo.com>
2021-02-25 17:43:38 +0330

    staff api added

--- components/landing/staff/StaffCard.vue
+++ components/landing/staff/StaffCard.vue
@@ -1,22 +1,30 @@
 <template>
-  <div class="staff_card d-inline-block" style="max-width:350px;min-width:290px">
+  <div class="staff_card" style="max-width:350px;min-width:290px">
     <div class="staff_img grey darken-3" style="margin:24px auto;height:230px;width:230px">
-      <img src="" alt="staff image" width="100%" height="100%" style="object-fit: cover;" />
+      <img :src="staff.image" alt="staff image" width="100%" height="100%" style="object-fit: cover;" />
     </div>
     <div class="staff_intro pb-4" style="width:230px; margin:5px auto;">
       <div class="font-weight-bold white--text">
-        پسر خوب
+        {{staff.first_name_en}} {{staff.last_name_fa}}
       </div>
       <div class="text-caption text-start" style="margin-top:12px">
-        برنامه نویس فرانت اند
+        {{staff.team_title}}
       </div>
     </div>
     <div class="primary py-1 text-center white--text font-weight-black">
-      فنی
+      {{staff.group_title}}
     </div>
   </div>
 </template>
-
+<script>
+export default {
+  props:['staff'],
+  data(){
+    console.log('sol:',this.staff)
+    return{}
+  }
+}
+</script>
 <style scoped>
 .staff_card {
   width: 100%;
