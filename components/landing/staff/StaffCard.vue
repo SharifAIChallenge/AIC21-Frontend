5bcc40304489af813eb72dd9510d62db5fcac2fe
saman-re <saman.rezapoor1379@yahoo.com>
2021-02-26 22:22:40 +0330

    staff card style improved

--- components/landing/staff/StaffCard.vue
+++ components/landing/staff/StaffCard.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="staff_card" style="max-width:350px;min-width:290px">
-    <div class="staff_img grey darken-3" style="margin:24px auto;height:230px;width:230px">
+  <div class="staff_card">
+    <div class="staff_img grey darken-3" style="margin:24px auto;height:230px;width:230px;position:relative">
       <img :src="`https://api-stg.aichallenge.ir/${staff.image}`" alt="staff image" width="100%" height="100%" style="object-fit: cover;" />
+    <div class="primary py-1 text-center white--text font-weight-black" style="position:absolute;width:30%;left:0;bottom:20px">
+      {{staff.group_title}}
+    </div>
     </div>
     <div class="staff_intro pb-4" style="width:230px; margin:5px auto;">
       <div class="font-weight-bold white--text">
         {{staff.first_name_en}} {{staff.last_name_fa}}
       </div>
-      <div class="text-caption text-start" style="margin-top:12px">
+      <div class="text-caption text-start primary--text" style="margin-top:12px;">
         {{staff.team_title}}
       </div>
-    </div>
-    <div class="primary py-1 text-center white--text font-weight-black">
-      {{staff.group_title}}
     </div>
   </div>
 </template>
@@ -23,8 +23,10 @@ export default {
 </script>
 <style scoped>
 .staff_card {
+  max-width:350px;
+  min-width:290px;
   width: 100%;
-  border: 2px solid var(--v-primary-base);
+  /* border: 2px solid var(--v-primary-base); */
 }
 .staff_intro,
 .staff_img {
