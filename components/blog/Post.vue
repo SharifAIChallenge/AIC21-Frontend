cc0657d3dc53821372f7676837280625b422c9c6
saman-re <saman.rezapoor1379@yahoo.com>
2021-03-01 22:28:43 +0330

    copy link added to _slug.vue btn

--- components/blog/Post.vue
+++ components/blog/Post.vue
@@ -1,6 +1,6 @@
 <template>
     <v-card class="mx-auto" max-width="800" tile flat :elevation="cardEval" @mousemove="cardEval=24" @mouseleave="cardEval=0" :to="`/blog/${post.id}`"> 
-      <v-img class="white--text align-end" height="200px" :src="`https://aichallenge.sharif.edu/${post.image}`">
+      <v-img class="white--text align-end" height="200px" :src="post.image">
       </v-img>
       <v-card-subtitle class="primary--text pb-1 pt-2 pr-1">
           {{
