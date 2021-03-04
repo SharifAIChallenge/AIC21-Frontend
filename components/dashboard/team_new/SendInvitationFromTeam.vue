0f2db3096dc94d62c56bd3ee8f2832c360001053
hessamasna <hessamasna@yahoo.com>
2021-03-05 00:09:46 +0330

    sendInvitation logic  already done! have problem with update list that backend should fix it first

---
+++ components/dashboard/team_new/SendInvitationFromTeam.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <div v-if="this.invitationsList.status_code === 200">
+      <h1>
+        <v-icon color="primary" size="40px" class="pl-2 pr-2">mdi-account-plus-outline</v-icon>
+        دعوت دیگران به تیم
+      </h1>
+      <v-alert color="black" dark icon="mdi-information" width="50%" dense>
+        اگر هم‌تیمی شما در سایت ثبت‌نلم کرده است، باوراد کردن ایمیل او را به تیمتان دعوت کنید.
+      </v-alert>
+      <v-form ref="form" v-model="valid">
+        <v-text-field
+          v-model="email"
+          label="آدرس ایمیل هم‌تیمی"
+          outlined
+          dir="ltr"
+          type="email"
+          :rules="[v => !!v || 'ایمیل نمی تواند خالی باشد!', v => /.+@.+/.test(v) || 'لطفا ایمیل وارد کنید!']"
+          required
+          height="36px"
+        ></v-text-field>
+        <div>
+          <v-btn width="50vw" height="60px" class="text-h6" color="primary">
+            <v-icon color="white" size="30px" class="pl-4 pr-2">mdi-script-outline</v-icon>
+            ارسال دعوتنامه
+          </v-btn>
+        </div>
+      </v-form>
+      <div>
+        <div class="pa-10">
+          <v-icon color="white" size="30px" class="pl-4 pr-2">mdi-script-text-outline</v-icon>
+          تاریخچه دعوت ها
+        </div>
+        <div v-for="(list, index) in invitationsList.data" :key="index">
+          <div>
+            <div>
+              {{ list.user.first_name }} {{ list.user.last_name }}
+              <v-icon :color="statusColor(list.status)" size="30px" class="pl-4 pr-2">{{ statusIcon(list.status) }}</v-icon>
+              {{ statusMessage(list.status) }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-else>
+      تیم شما پر است
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  auth: false,
+    async fetch() {
+      await this.$axios.$get('team/invitations/team_sent').then(res => {
+        this.invitationsList = res;
+      });
+    },
+  data() {
+    return {
+      email: '',
+      valid: false,
+      invitationsList: {
+        // data: [
+        //   {
+        //     user: {
+        //       first_name: 'علی',
+        //       last_name: 'علی زاده',
+        //       email: 'user1@user1.com',
+        //     },
+        //     status: 'accepted',
+        //   },
+        // ],
+        // status_code: 200,
+      },
+    };
+  },
+  methods: {
+    statusIcon(status) {
+      if (status === 'pending') return 'mdi-progress-question';
+      else if (status === 'accepted') return 'mdi-progress-check';
+      else if (status === 'rejected') return 'mdi-progress-close';
+    },
+    statusColor(status) {
+      if (status === 'pending') return 'blue';
+      else if (status === 'accepted') return 'green';
+      else if (status === 'rejected') return 'orange';
+    },
+    statusMessage(status) {
+      if (status === 'pending') return 'در انتظار پاسخ';
+      else if (status === 'accepted') return 'قبول کرد';
+      else if (status === 'rejected') return 'رد کرد';
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ pages/test2.vue
@@ -0,0 +1,15 @@
+<template>
+  <SendInvitationFromTeam />
+</template>
+
+<script>
+import SendInvitationFromTeam from '../components/dashboard/team_new/SendInvitationFromTeam';
+
+export default {
+  layout: 'landing',
+  components: { SendInvitationFromTeam },
+  auth: false,
+};
+</script>
+
+<style></style>
