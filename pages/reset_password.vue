9d63ab2e5894e0d5d04eea7d84165e35f62c1d85
jamshidi.m799 <jamshidi.m799@gmail.com>
2021-03-11 15:08:44 +0330

    add reset password page

--- pages/reset_password.vue
+++ pages/reset_password.vue
@@ -1,113 +1,12 @@
-<template>
-  <div>
-    <v-row justify="end" align="center" style="height: 100px" class="mt-n3 px-3 px-sm-5">
-      <v-btn rounded text large to="/forgot_password" icon>
-        <v-icon>mdi-arrow-left</v-icon>
-      </v-btn>
-    </v-row>
-    <v-row justify="center" align="center" class="my-3">
-      <v-col cols="12" sm="7" md="5" lg="3">
-        <v-row justify="center" align="baseline" class="text-uppercase">
-          <glow>
-            <span class="display-2 white--text">
-              {{ $t('form.change') }}
-            </span>
-          </glow>
-        </v-row>
-        <v-row justify="center" class="mb-5 mt-1">
-          <glow>
-            <span class="display-3 secondary--text font-weight-black mx-1">
-              {{ $t('form.password') }}
-            </span>
-          </glow>
-        </v-row>
-        <v-form ref="form" v-model="valid" @submit.prevent="resetPasswordConfirm">
-          <password-input v-model="passes.new_password1" label="form.newPassword" @vis-change="val => (show = val)" />
-
-          <v-text-field
-            v-model="passes.new_password2"
-            :label="$t('form.confirmNewPassword')"
-            :type="show ? 'text' : 'password'"
-            :rules="requiredRules"
-            required
-            v-bind="filedProps"
-            dir="ltr"
-          />
-
-          <v-btn
-            :disabled="!valid || passes.new_password1 !== passes.new_password2"
-            :loading="loading"
-            type="submit"
-            v-bind="primaryButtonProps"
-          >
-            <v-icon left>mdi-shield-refresh-outline</v-icon>
-            {{ $t('form.changePassword') }}
-          </v-btn>
-        </v-form>
-      </v-col>
-    </v-row>
-  </div>
-</template>
+<template></template>
 
 <script>
-import { requiredRules } from '../mixins/formValidations';
-import { primaryButtonProps } from '../mixins/buttonProps';
-import { fieldProps } from '../mixins/fieldProps';
-import Glow from '../components/Glow';
-import PasswordInput from '../components/PasswordInput';
-import { RESET_PASSWORD, RESET_PASSWORD_CONFIRM ,resetPasswordConfirm } from '../api';
-
 export default {
-  auth: 'guest',
   layout: 'form',
-  components: { PasswordInput, Glow },
-  mixins: [requiredRules, primaryButtonProps, fieldProps],
-  validate({ query }) {
-    return query.uid && query.token;
-  },
-  data() {
-    return {
-      valid: false,
-      passes: {
-        new_password1: '',
-        new_password2: '',
-        uid: this.$route.query.uid,
-        token: this.$route.query.token,
-      },
-      show: false,
-      loading: false,
-    };
-  },
-  methods: {
-    async resetPasswordConfirm() {
-      // const config = {
-      //   url: RESET_PASSWORD_CONFIRM.url,
-      //   method: RESET_PASSWORD_CONFIRM.method,
-      //   headers: {
-      //     Authorization: false,
-      //   },
-      //   [RESET_PASSWORD_CONFIRM.payload]: {
-      //     new_password1: this.new_password1,
-      //     new_password2: this.new_password2,
-      //     uid: this.$route.query.uid,
-      //     token: this.$route.query.token,
-      //   },
-      // };
-
-      this.loading = true;
-      let { data } = await resetPasswordConfirm(this.$axios, this.passes);
-      this.loading = false;
-      if (data.status_code) {
-        if (data.status_code === 200) {
-          this.$toast.success('رمز عبور با موفقیت تغییر یافت.');
-          this.$router.push('/login');
-        } else {
-          this.$toast.error('خطا در تثییر رمز عبور.');
-        }
-      }
-    },
+  mounted() {
+    console.log('1');
+    this.$store.commit('formStatus/toggleShow');
+    this.$store.commit('formStatus/changeStatus', 'reset');
   },
 };
 </script>
-
-<style scoped></style>
