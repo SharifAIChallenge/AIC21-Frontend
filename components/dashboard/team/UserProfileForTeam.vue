f1e9e22e07f774ce906bde1231655ed29bc5e3ad
hessamasna <hessamasna@yahoo.com>
2021-03-12 19:09:30 +0330

    User Profile Done!

--- components/dashboard/team/UserProfileForTeam.vue
+++ components/dashboard/team/UserProfileForTeam.vue
@@ -1,7 +1,24 @@
 <template>
   <v-card>
     <img v-if="userData.profile.image !== null" :src="userData.profile.image" width="100%" :alt="userData.profile.name" />
-    <div class="pa-2 pt-1">{{ userData.profile.firstname_fa }} {{ userData.profile.lastname_fa }}</div>
+    <div class="pa-2 pt-1">
+      {{ userData.profile.firstname_fa }} {{ userData.profile.lastname_fa }}
+      <v-btn :href="userData.profile.github" target="_blank">
+        <v-icon :disabled="userData.profile.linkedin === null">
+          mdi-linkedin
+        </v-icon>
+      </v-btn>
+      <v-btn :href="userData.profile.github" target="_blank">
+        <v-icon :disabled="userData.profile.github === null">
+          mdi-github
+        </v-icon>
+      </v-btn>
+      <v-btn :href="userData.profile.resume" target="_blank">
+        <v-icon :disabled="userData.profile.resume === null">
+          mdi-file-account
+        </v-icon>
+      </v-btn>
+    </div>
     <v-container class="pt-0 mt-0">
       <div class="pr-8 pb-4">
         <ul>
@@ -12,32 +29,9 @@
           <li>ترم: {{ userData.profile.university_term }}</li>
         </ul>
       </div>
-      <div class="social">
-        <div>
-          <a :href="userData.profile.linkedin" target="_blank">
-            <v-icon :disabled="userData.profile.linkedin === null">
-              mdi-linkedin
-            </v-icon>
-          </a>
-        </div>
-        <div>
-          <v-btn :href="userData.profile.github" target="_blank">
-            <v-icon :disabled="userData.profile.github === null" >
-              mdi-github
-            </v-icon>
-          </v-btn>
-        </div>
-        <div>
-          <v-btn :href="userData.profile.resume" target="_blank">
-            <v-icon :disabled="userData.profile.resume === null">
-              mdi-file-account
-            </v-icon>
-          </v-btn>
-        </div>
-      </div>
     </v-container>
     <div class="" v-if="userData.show">
-      <v-btn width="100%" height="55px" class="text-h6" color="primary" @click="sendInvitation(userEmail)">
+      <v-btn width="100%" height="55px" class="text-h6" color="primary" @click="sendInvitation(userEmail)" block :loading="loadingBtn">
         <v-icon color="white" size="30px" class="pl-2 pr-2">mdi-script-outline</v-icon>
         ارسال دعوتنامه
       </v-btn>
@@ -48,18 +42,16 @@
 <script>
 export default {
   props: ['userData'],
-  // async fetch() {
-  //   this.$axios.$get(`accounts/profile/${this.id}`).then(res => {
-  //     console.log(res);
-  //   });
-  // },
   data() {
     return {
-      data: {},
+      data: {
+        loadingBtn: false,
+      },
     };
   },
   methods: {
     sendInvitation(email) {
+      this.loadingBtn = true;
       let user_email = email;
       this.$axios.$post('team/invitations/team_sent', { user_email }).then(res => {
         if (res.status_code === 200) {
@@ -68,18 +60,15 @@ export default {
           this.$toast.error(this.translateResponseMessage(res.message));
         }
       });
+      this.loadingBtn = false;
     },
     translateResponseMessage(response) {
       if (response === 'your invitation sent') return 'دعوت نامه ارسال شد!';
-      else return 'مشکلی در ارسال دعوت نامه رخ داد! لطفا ایمیل را چک کنید!';
+      else if (response === 'you have a sent an invitation already') return 'شما قبلا دعوت نامه ارسال کردید!';
+      else return 'مشکلی در ارسال دعوت نامه رخ داد!';
     },
   },
 };
 </script>
 
-<style scoped lang="scss">
-.social {
-  display: flex;
-  justify-content: space-evenly;
-}
-</style>
+<style scoped lang="scss"></style>
