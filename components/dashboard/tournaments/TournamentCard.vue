992db36ef6e76081898ea24bb6adda36ea16684c
mansoureh mahzadeh <mansoureh.1998@gmail.com>
2021-03-21 23:39:21 +0330

    add tournamentcards

---
+++ components/dashboard/tournaments/TournamentCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div :class="[tournament.finished ? 'active' : '' , 'tournamentCard d-flex flex-column pa-4']">
+    <h3 class="mb-4">{{tournament.title}}</h3>
+    <div class="mb-4">
+      <span>{{tournament.date}}</span>
+      <span> - ساعت</span>
+      <span>{{tournament.time}}</span>
+    </div>
+    <v-btn class="py-5" style="border:2px solid" v-if="tournament.finished" to=/dashboard/scoreboard >
+    مشاهده جدول امتیازات
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tournament: Object,
+  },
+};
+</script>
+<style lang="scss">
+.tournaments {
+  background-color: #0e1224;
+  // border: 2px solid red;
+}
+.tournament {
+  .tournamentCard {
+    // border: 1px solid blue;
+    height: 170px;
+    width: 100%;
+    background-color: #141432;
+    a {
+      background-color: #1a1940 !important;
+    }
+  }
+  .active {
+    background-color: #1a1940;
+  }
+}
+</style>
--- pages/dashboard/tournaments.vue
+++ pages/dashboard/tournaments.vue
@@ -3,6 +3,13 @@
     <SectionHeader title="تورنومنت ها" icon="mdi-tournament" />
     <div class="px-6 px-md-12">
       <TournamentHeader :header="header"></TournamentHeader>
+      <div class="py-8 d-flex flex-wrap justify-space-between">
+        <div class="tournament pa-0 my-4" v-for="tournament in tournaments" :key="tournament.date">
+          <div>
+            <TournamentCard :tournament="tournament"></TournamentCard>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -11,9 +18,10 @@
 import SectionHeader from '~/components/SectionHeader';
 import SectionContainer from '~/components/SectionContainer';
 import TournamentHeader from '~/components/dashboard/tournaments/TournamentHeader';
+import TournamentCard from '~/components/dashboard/tournaments/TournamentCard';
 
 export default {
-  components: { SectionHeader, SectionContainer, TournamentHeader },
+  components: { SectionHeader, SectionContainer, TournamentHeader, TournamentCard },
   layout: 'dashboard',
   transition: 'fade-transition',
   data() {
@@ -24,9 +32,46 @@ export default {
         time: '23:00:00',
         deadline: '2021-03-22T20:00:00',
       },
+      tournaments: [
+        {
+          title: 'تورنومنتی که تمام شده',
+          date: '1400/01/10',
+          time: '23:00:00',
+          deadline: '2021-03-22T20:00:00',
+          finished: true,
+        },
+        {
+          title: 'تورنومنتی که تمام شده',
+          date: '1400/01/10',
+          time: '23:00:00',
+          deadline: '2021-03-22T20:00:00',
+          finished: true,
+        },
+        {
+          title: 'تورنومنتی که شروع نشده',
+          date: '1400/01/10',
+          time: '23:00:00',
+          deadline: '2021-03-22T20:00:00',
+          finished: false,
+        },
+        {
+          title: 'تورنومنتی که شروع نشده',
+          date: '1400/01/10',
+          time: '23:00:00',
+          deadline: '2021-03-22T20:00:00',
+          finished: false,
+        },
+      ],
     };
   },
 };
 </script>
 <style lang="scss">
+@import '~/assets/mixins.scss';
+.tournament {
+  width: 32%;
+  @include v-not-sm {
+    width: 100%;
+  }
+}
 </style>
