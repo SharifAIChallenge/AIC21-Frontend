c74c548738c42f07146be8ff88789206217fb1a8
jamshidi.m799 <jamshidi.m799@gmail.com>
2021-04-19 13:17:50 +0430

    fix statistic style

--- components/landing/Statistics.vue
+++ components/landing/Statistics.vue
@@ -1,14 +1,14 @@
 <template>
   <v-card tile flat class="secondary my-5 py-md-15 py-3">
-    <div class="d-flex justify-start align-center flex-wrap px-5 px-md-12">
-      <v-col cols="6" md="3" v-for="(stat, i) in stats" :key="i">
+    <v-row class="d-flex justify-center align-center flex-wrap px-5 px-md-12">
+      <v-col md="4" xl="2" v-for="(stat, i) in stats" :key="i">
         <v-card-text class="pl-md-15 py-md-5 p-0 text-right mx-auto">
           <v-icon size="75px" right class="mb-7 black--text align-right statisticsIcon">{{ stat.icon }}</v-icon>
           <h1 class="part mb-12 mt-5 font-weight-bold">{{ stat.stat }}</h1>
           <span class="pt-1 text-md-h4 text-h5 statisticsTitle" style="border-top:2px solid white">{{ stat.title }}</span>
         </v-card-text>
       </v-col>
-    </div>
+    </v-row>
   </v-card>
 </template>
 <script>
